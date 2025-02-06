8410564a6c4fa49998b738c5ffba4ccfab534141
Olalekan Bakare <peterubakare@gmail.com>
2025-02-06 12:02:27 +0100

    Add the string file

--- Copy of codes/number-and-math.js
+++ Copy of codes/number-and-math.js
@@ -1,3 +1,6 @@
+//On the browser type supersimple.dev/projects/amazon. (This is the link to the final project we will create
+// in this course)
+
 // Basics of Math:
 2 + 2
 10 - 3
@@ -7,10 +10,12 @@
 2 + 2 + 2
 2.2 + 2.2
 
+
 10.90 * 2 + 20.95
 10.90 * 2 + 20.95 + 4.99
 
-// Order of operations:
+// Order of operations:  multiplication and division have the same priority
+//If we have both in a caculation, it will calculate from left to right same for addition and subtraction too
 1 + 1 * 3
 (1 + 1) * 3
 
@@ -23,9 +28,13 @@
 20.95 + 7.99
 (2095 + 799) / 100
 
-// How to round numbers:
-Math.round(2.2)
-Math.round(2.8)
+// How to round numbers: It means taking a number and moving it to the nearest integer
+Math.round(2.2) // output will be 2
+Math.round(2.8) // output will be 3
 
 ((2095 + 799) * 0.1) / 100
 Math.round((2095 + 799) * 0.1) / 100
+
+// Using JavaScript to convert temperature from Celsius to Fahrenheit. The formula is:
+// Fahrenheit = (Celsius * 9/5) + 32
+// Celsius = (Fahrenheit - 32) * 5/9
---
+++ Copy of codes/strings.js
@@ -0,0 +1,38 @@
+// Strings represent text
+
+'hello'
+alert('hello');
+
+//Combining strings together is called concatenation
+'some' + 'text'  //output will be 'sometext'
+'some' + 'more' + 'text' 
+
+typeof 2    // to check the type of vale 2 is. The output = 'number'
+
+typeof 'hello'   // output = 'string'
+
+'hello' + 3     // output = 'hello3'.     JavaSript automatically convert it to a string
+
+'$' + 20.95 + 7.99     // Output = '$20.957.99'      Find the best way to do this below.
+
+'$' + (20.95 + 7.99)   //  Output =  '$28.939999999999998' This math is inaccurate
+// When calculating money, the best way is to always calculate in cents then you convert to dollars
+
+'$' + (2095 + 799) / 100  // Output = '$28.94'
+
+'Items (' + (1 + 1) + '): $' + (2095 + 799) / 100  // Output = 'Items (2): $28.94'
+
+
+alert('Items (' + (1 + 1) + '): $' + (2095 + 799) / 100);  
+
+
+"hello"
+"I'm learning JavaScript"
+
+'I\'m learning JavaScript'
+alert('some\ntext');
+
+`hello`
+`Items (${1 + 1}): $${(2095 + 799) / 100}`
+`some
+text`
